d0d4c30db74bf3cdbe66928ff0f1103a7c439cc7
MarvinHauke <marvin-hauke@hotmail.de>
2024-07-27 12:57:23 +0200

    refactored PowerShell profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,48 +1,46 @@
-Set-PSReadlineOption -PredictionViewStyle ListView #TODO: add jk selection for suggestions
+
+# Enable ListView for PSReadline and set vi mode
+Set-PSReadlineOption -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Key 'ctrl+k' -Function NextHistory
 Set-PSReadLineKeyHandler -Key 'ctrl+j' -Function PreviousHistory
 Set-PSReadlineOption -EditMode Vi
 
-# import several Modules
+# Import necessary modules
 Import-Module posh-git
 Import-Module PSScriptAnalyzer
 Import-Module PSReadLine
 
-
-# This example emits a cursor change VT escape in response to a Vi mode change.
+# Function to handle Vi mode cursor change
 function OnViModeChange {
   if ($args[0] -eq 'Command') {
-    # Set the cursor to a blinking block.
-    Write-Host -NoNewLine "`e[1 q"
+    Write-Host -NoNewLine "`e[1 q"  # Blinking block cursor
   } else {
-    # Set the cursor to a blinking line.
-    Write-Host -NoNewLine "`e[5 q"
+    Write-Host -NoNewLine "`e[5 q"  # Blinking line cursor
   }
 }
 Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
-# Set Autopair quotes 
+
+# Set autopair quotes
 Set-PSReadLineKeyHandler -Chord '"',"'" `
   -BriefDescription SmartInsertQuote `
   -LongDescription "Insert paired quotes if not already on a quote" `
   -ScriptBlock {
   param($key, $arg)
-
   $line = $null
   $cursor = $null
   [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-
   if ($line.Length -gt $cursor -and $line[$cursor] -eq $key.KeyChar) {
-    # Just move the cursor
     [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
   } else {
-    # Insert matching quotes, move cursor to be in between the quotes
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)" * 2)
     [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
     [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor - 1)
   }
 }
+
+# Function to run a command as admin
 function admin {
-  if ($args.Count -gt 0) {   
+  if ($args.Count -gt 0) {
     $argList = "& '" + $args + "'"
     Start-Process "$psHome\pwsh.exe" -Verb runAs -ArgumentList $argList
   } else {
@@ -52,67 +50,61 @@ function admin {
 Set-Alias -Name su -Value admin
 Set-Alias -Name sudo -Value admin
 
+# Function to find command path
 function which($name) {
   Get-Command $name | Select-Object -ExpandProperty Definition
 }
 
+# Function to find a file recursively
 function find-file($name) {
-  Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
-    $place_path = $_.directory
+  Get-ChildItem -Recurse -Filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+    $place_path = $_.DirectoryName
     Write-Output "${place_path}\${_}"
   }
 }
 
+# Function to grep content in files
 function grep($regex, $dir) {
-  if ( $dir ) {
-    Get-ChildItem $dir | select-string $regex
-    return
+  if ($dir) {
+    Get-ChildItem $dir | Select-String $regex
+  } else {
+    $input | Select-String $regex
   }
-  $input | select-string $regex
 }
 
-# Add path to your Development Directory
+# Function to change to development directory
 function go_to_dev {
-  [CmdletBinding()]
   param (
     [Parameter(Position = 0, Mandatory = $false, ValueFromPipeline = $true)]
     [String]$Directory
   )
-
   $path = Join-Path $HOME\Development\ $Directory
-
   if (Test-Path -Path $path -PathType Container) {
     Set-Location -Path $path
   } else {
     Write-Host "Directory '$Directory' not found in $HOME\Development/."
   }
 }
+Set-Alias -Name 'cdd' -Value go_to_dev
 
-# Define a custom argument completer for the 'Path' parameter
+# Argument completer for the 'Path' parameter
 $CompletePath = {
   param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
-
-  # Get suggestions for partly typed paths
-  $suggestions = Get-ChildItem -Path $wordToComplete -Directory | Select-Object -ExpandProperty Name
-
-  foreach ($suggestion in $suggestions) {
-    if ($suggestion.StartsWith($wordToComplete, [StringComparison]::InvariantCultureIgnoreCase)) {
-      $suggestion
+  Get-ChildItem -Path $wordToComplete -Directory | Select-Object -ExpandProperty Name | ForEach-Object {
+    if ($_.StartsWith($wordToComplete, [StringComparison]::InvariantCultureIgnoreCase)) {
+      $_
     }
   }
 }
-# Register the argument completer for the 'Path' parameter of your function
-Set-Alias -Name 'cdd' -Value go_to_dev
 Register-ArgumentCompleter -CommandName 'cdd' -ParameterName 'Path' -ScriptBlock $CompletePath
 
+# Git alias and function
 function cfg {
   param(
     [Parameter(Position = 0, Mandatory = $false, ValueFromRemainingArguments = $true)]
     [String[]]$AdditionalArgs
   )
-
   $gitCommand = "git --git-dir=`$HOME/.cfg/ --work-tree=`$HOME"
-
   $commitIndex = $AdditionalArgs.IndexOf('-m')
   if ($commitIndex -ne -1) {
     $commitMessage = $AdditionalArgs[$commitIndex + 1]
@@ -125,68 +117,67 @@ function cfg {
 }
 Set-Alias -Name config -Value cfg
 
-
-# Show all tracked files from config
-function showConfigFiles{
+function showConfigFiles {
   git --git-dir=$HOME/.cfg/ ls-tree -r master --name-only
 }
 
-function LazyGitFunc{
+function LazyGitFunc {
   $lazyGitCommand = "lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME"
   if (Get-Command lazygit -ErrorAction SilentlyContinue) {
     Invoke-Expression $lazyGitCommand
-  } else{
+  } else {
     Write-Host "LazyGit not installed"
   }
 }
-Set-Alias -Name lzconf -Value lazyGitFunc
+Set-Alias -Name lzconf -Value LazyGitFunc
 
-# Set vim to nvim if installed
-if ((Get-Command nvim -ErrorAction Ignore)) {
+# Nvim configuration functions
+if (Get-Command nvim -ErrorAction Ignore) {
   Set-Alias -Name vim -Value nvim
   Write-Host "vim is now nvim"
 } else {
-  Write-Host "Nvim is not installed!"
+  Write-Host "Nvim is not installed!" -ForegroundColor Red
 }
 
-
-function NvimConfigFunc{
-  $nvim_config_path = "$HOME/AppData/Local/nvim/"
-  if (Test-Path -Path $nvim_config_path -PathType Container){
-    $nvconfCommand =  "vim " + $nvim_config_path
-    Invoke-Expression $nvconfCommand
+function NvimConfigFunc {
+  $nvim_config_path = "$HOME\AppData\Local\nvim\"
+  if (Test-Path -Path $nvim_config_path -PathType Container) {
+    Invoke-Expression "vim $nvim_config_path"
+  } else {
+    Write-Host "Nvim configuration directory not found!" -ForegroundColor Red
   }
 }
 Set-Alias -Name nvc -Value NvimConfigFunc
 
-function NvimConfigFolder{
-  $nvim_config_path = "$HOME/AppData/Local/nvim/"
-  if (Test-Path -Path $nvim_config_path -PathType Container){
-    $nvconfCommand =  "cd " + $nvim_config_path
-    Invoke-Expression $nvconfCommand
+function NvimConfigFolder {
+  $nvim_config_path = "$HOME\AppData\Local\nvim\"
+  if (Test-Path -Path $nvim_config_path -PathType Container) {
+    Set-Location $nvim_config_path
+  } else {
+    Write-Host "Nvim configuration directory not found!" -ForegroundColor Red
   }
 }
 Set-Alias -Name cdn -Value NvimConfigFolder
 
-function NotesFolder{
+# Notes directory function
+function NotesFolder {
   $notesPath = "$HOME\Documents\Notizen"
-  if (Test-Path -Path $notesPath -PathType Container){
-    Set-Location "$notesPath"
+  if (Test-Path -Path $notesPath -PathType Container) {
+    Set-Location $notesPath
+  } else {
+    Write-Host "Notes directory not found!" -ForegroundColor Red
   }
 }
 Set-Alias -Name notes -Value NotesFolder
 
-function ProfileConfigFunc{
-  vim $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+# Edit profile configuration function
+function ProfileConfigFunc {
+  vim "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
 }
 Set-Alias -Name nvp -Value ProfileConfigFunc
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
+# Import Chocolatey profile for tab completion
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+if (Test-Path $ChocolateyProfile) {
+  Import-Module $ChocolateyProfile
 }
