3b649ef8a291ec96d17920ea2e659f16e4b8816e
PfoRsten <marvin-hauke@hotmail.de>
2025-03-26 02:24:20 +0100

    fixed edgecases for bufdelete with nvim-tree and last buffer open

--- .config/nvim/lua/plugins/bufdelete.lua
+++ .config/nvim/lua/plugins/bufdelete.lua
@@ -10,11 +10,22 @@ return {
 			if #listed_buffers > 1 then
 				vim.cmd("Bdelete")
 			else
-				local last_buf_name = vim.api.nvim_buf_get_name(listed_buffers[1])
+				-- If only one buffer is left, check its name
+				local last_buf = listed_buffers[1] or buffers[1] -- Fallback to any buffer if needed
+				local last_buf_name = last_buf and vim.api.nvim_buf_get_name(last_buf) or ""
+
+				-- Get the number of listed buffers and check if nvim-tree is the only one left
+				local open_windows = vim.api.nvim_tabpage_list_wins(0)
+				local nvim_tree_only = #open_windows == 1
+					and vim.bo[vim.api.nvim_win_get_buf(open_windows[1])].filetype == "NvimTree"
+
 				if last_buf_name == "" then -- Empty name means "No Name"
 					vim.cmd("quit")
-				else
+					vim.cmd("quit") -- do a second quit for also leaving nvim-tree
+				elseif nvim_tree_only then
 					vim.cmd("quit")
+				else -- Otherwise, just delete the buffer
+					vim.cmd("Bdelete")
 				end
 			end
 		end, {})
