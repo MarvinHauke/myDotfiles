07ba9f1dbf371d7e21a0610818991f740a50c4b2
PfoRsten <marvin-hauke@hotmail.de>
2025-04-13 15:39:14 +0200

    reoved .arduino15 and added ruff in faor of pyright

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -23,7 +23,6 @@ return {
 				"bashls",
 				"lua_ls",
 				"clangd",
-				"arduino_language_server",
 				"jsonls",
 				"lemminx",
 				"marksman",
@@ -35,6 +34,7 @@ return {
 				"html",
 				"svelte",
 				"rust_analyzer",
+				"ruff",
 			},
 			automatic_installation = true,
 		})
@@ -163,68 +163,16 @@ return {
 			},
 		})
 
-		-- -- Arduino LSP settings
-		-- -- When the arduino server starts in these directories, use the provided FQBN.
-		-- -- Note that the server needs to start exactly in these directories.
-		-- -- This example would require some extra modification to support applying the FQBN on subdirectories!
+		-- Python ruff linter
+		lspconfig.ruff.setup({
+			capabilities = lsp_capabilities,
+			on_attach = lsp_attach,
+		})
 
-		--
-		-- -- Define FQBN mappings for different projects
-		-- local my_arduino_fqbn = {
-		-- 	["teensy31"] = "teensy:avr:teensy31",
-		-- 	["teensy41"] = "teensy:avr:teensy41",
-		-- 	["esp32dev"] = "esp32:esp32:esp32",
-		-- }
-		--
-		-- -- Default to Arduino Nano if no board is detected
-		-- local DEFAULT_FQBN = "arduino:avr:nano"
-		--
-		-- -- Function to get the board from platformio.ini
-		-- local function get_fqbn_from_pio()
-		-- 	local ini_file = io.open("platformio.ini", "r")
-		-- 	if not ini_file then
-		-- 		return DEFAULT_FQBN
-		-- 	end
-		--
-		-- 	for line in ini_file:lines() do
-		-- 		local board = line:match("^%s*board%s*=%s*(%S+)")
-		-- 		if board and my_arduino_fqbn[board] then
-		-- 			ini_file:close()
-		-- 			return my_arduino_fqbn[board]
-		-- 		end
-		-- 	end
-		--
-		-- 	ini_file:close()
-		-- 	return DEFAULT_FQBN
-		-- end
-		--
-		-- -- LSP Setup for Arduino
-		-- lspconfig.arduino_language_server.setup({
+		-- -- Python LSP settings (removed in favor of ruff)
+		-- lspconfig.pyright.setup({
 		-- 	capabilities = lsp_capabilities,
 		-- 	on_attach = lsp_attach,
-		--
-		-- 	on_new_config = function(config, root_dir)
-		-- 		local fqbn = get_fqbn_from_pio()
-		-- 		vim.notify(("Using FQBN: %q for project in %q"):format(fqbn, root_dir))
-		--
-		-- 		config.cmd = {
-		-- 			"arduino-language-server",
-		-- 			"-clangd",
-		-- 			"/usr/bin/clangd", -- Ensure clangd is installed
-		-- 			"-cli",
-		-- 			"/opt/homebrew/bin/arduino-cli", -- Homebrew-installed Arduino CLI
-		-- 			"-cli-config",
-		-- 			"/Users/pforsten/Library/Arduino15/arduino-cli.yaml",
-		-- 			"-fqbn",
-		-- 			fqbn,
-		-- 		}
-		-- 	end,
-		--
-		-- 	filetypes = { "ino" },
-		--
-		-- 	root_dir = function(fname)
-		-- 		return util.root_pattern("*.ino", "CMakeLists.txt", ".git", "platformio.ini")(fname) or vim.fn.getcwd() -- Fallback to the current working directory
-		-- 	end,
 		-- })
 
 		lspconfig.rust_analyzer.setup({
@@ -311,12 +259,6 @@ return {
 					},
 				},
 			},
-		})
-
-		-- Python LSP settings
-		lspconfig.pyright.setup({
-			capabilities = lsp_capabilities,
-			on_attach = lsp_attach,
 		})
 
 		-- CMakeLists
