84a6650b0798eb7f9ff3eda9de4c147ecd2781ae
PfoRsten <marvin-hauke@hotmail.de>
2025-04-24 14:37:11 +0200

    added color support for hex, rgb, css etc.

---
+++ .config/nvim/lua/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,6 @@
+return {
+	-- additionaly add the setup() in core/options.lua under
+	-- opt.termguicolors = true
+	-- require("nvim-highlight-colors").setup({})
+	"brenoprata10/nvim-highlight-colors",
+}
