55a630ff325748b7b111cc94b6692b044f8e51a8
PfoRsten <marvin-hauke@hotmail.de>
2025-11-05 15:07:06 +0100

    added markdown preview for vim and config for karabiner

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -10,21 +10,21 @@ return {
 	},
 
 	-- install without yarn or npm
+	-- {
+	-- 	"iamcco/markdown-preview.nvim",
+	-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	-- 	ft = { "markdown" },
+	-- 	build = function()
+	-- 		vim.fn["mkdp#util#install"]()
+	-- 	end,
+	-- },
 	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
 		end,
+		ft = { "markdown" },
 	},
-	-- {
-	--   "iamcco/markdown-preview.nvim",
-	--   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	--   build = "cd app && yarn install",
-	--   init = function()
-	--     vim.g.mkdp_filetypes = { "markdown" }
-	--   end,
-	--   ft = { "markdown" },
-	-- },
 }
