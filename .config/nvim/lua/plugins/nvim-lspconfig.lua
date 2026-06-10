b7c86921f40bd8b1126d5c4c1805ddbf754bb90e
PfoRsten <marvin-hauke@hotmail.de>
2026-06-10 02:12:04 +0200

    refactor(raspbian): trim LSP servers and tools for Pi

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,51 +1,37 @@
 return {
-  -- LSP Configuration
-  "neovim/nvim-lspconfig", -- https://github.com/neovim/nvim-lspconfig
+  "neovim/nvim-lspconfig",
   event = "VeryLazy",
   dependencies = {
-    -- LSP Management
-    "williamboman/mason.nvim",           -- https://github.com/williamboman/mason.nvim
-    "williamboman/mason-lspconfig.nvim", -- https://github.com/williamboman/mason-lspconfig.nvim
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-
-    -- Useful status updates for LSP
-    "j-hui/fidget.nvim", -- https://github.com/j-hui/fidget.nvim
-
-    -- Additional lua configuration, makes nvim stuff amazing!
-    "folke/neodev.nvim", -- https://github.com/folke/neodev.nvim
+    "j-hui/fidget.nvim",
+    "folke/neodev.nvim",
   },
   config = function()
     require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = {
-        -- Update this list to the language servers you need installed
         "bashls",
         "cssls",
-        "tailwindcss",
         "html",
-        "svelte",
         "lua_ls",
         "jsonls",
-        "lemminx",
         "marksman",
-        "quick_lint_js",
-        "powershell_es",
         "pyright",
-        "lemminx",
         "clangd",
+        "ts_ls",
       },
       automatic_installation = true,
     })
+
     local lspconfig = require("lspconfig")
-    local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities() -- INFO Search for difference between default_capabilities and normal capabilities
+    local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
     local mason_tool_installer = require("mason-tool-installer")
     require("fidget").setup({})
 
-    local lsp_attach = function(client, bufnr)
-      -- Create your attached keybindings here..
-    end
+    local lsp_attach = function(client, bufnr) end
 
-    -- Call setup on each LSP server
     require("mason-lspconfig").setup_handlers({
       function(server_name)
         lspconfig[server_name].setup({
@@ -57,28 +43,25 @@ return {
 
     mason_tool_installer.setup({
       ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua",   -- lua formatter
-        "isort",    -- python formatter
-        "black",    -- python formatter
-        "pylint",   -- python linter
-        "prittier", -- js formatter
-        "eslint_d", -- js linter
+        "prettier",
+        "stylua",
+        "ruff",
+        "eslint_d",
+        "shellcheck",
+        "shfmt",
+        "clang-format",
       },
     })
 
-    -- Lua LSP settings
     lspconfig.lua_ls.setup({
       capabilities = lsp_capabilities,
       on_attach = lsp_attach,
-      settings = { -- custom settings for lua
+      settings = {
         Lua = {
-          -- make language server recognize the `vim` global
           diagnostics = {
             globals = { "vim" },
           },
           workspace = {
-            -- make language server aware of runtime files
             [vim.fn.expand("$VIMRUNTIME/lua")] = true,
             [vim.fn.stdpath("config") .. "/lua"] = true,
           },
@@ -86,44 +69,26 @@ return {
       },
     })
 
-    -- Bash LSP settings
     lspconfig.bashls.setup({
       capabilities = lsp_capabilities,
       on_attach = lsp_attach,
     })
 
-    -- PowerShell LSP settings
-    lspconfig.powershell_es.setup({
-      capabilities = lsp_capabilities,
-      on_attach = lsp_attach,
-      settings = { -- custom settings for powershell
-        powershell = {
-          codeFormatting = {
-            Preset = "OTBS",
-          },
-        },
-      },
-    })
-
-    -- HTML LSP settings
     lspconfig.html.setup({
       capabilities = lsp_capabilities,
       on_attach = lsp_attach,
     })
 
-    -- C++ LSP settings
     lspconfig.clangd.setup({
       capabilities = lsp_capabilities,
       on_attach = lsp_attach,
     })
 
-    -- JS LSP settings
     lspconfig.ts_ls.setup({
       capabilities = lsp_capabilities,
       on_attach = lsp_attach,
     })
 
-    -- Python LSP settings
     lspconfig.pyright.setup({
       capabilities = lsp_capabilities,
       on_attach = lsp_attach,
