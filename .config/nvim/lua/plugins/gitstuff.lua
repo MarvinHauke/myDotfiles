1892ae7e992fea927e631459c7754c8e04f1fcdb
PfoRsten <marvin-hauke@hotmail.de>
2026-06-10 02:12:03 +0200

    refactor(raspbian): remove vim-fugitive keep gitsigns only

--- .config/nvim/lua/plugins/gitstuff.lua
+++ .config/nvim/lua/plugins/gitstuff.lua
@@ -1,8 +1,5 @@
 return {
   {
-    "tpope/vim-fugitive",
-  },
-  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup({
@@ -14,19 +11,19 @@ return {
           changedelete = { text = "~" },
           untracked = { text = "┆" },
         },
-        signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-        numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
-        linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+        signcolumn = true,
+        numhl = false,
+        linehl = false,
+        word_diff = false,
         watch_gitdir = {
           follow_files = true,
         },
         auto_attach = true,
         attach_to_untracked = true,
-        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame = false,
         current_line_blame_opts = {
           virt_text = true,
-          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+          virt_text_pos = "eol",
           delay = 1000,
           ignore_whitespace = false,
           virt_text_priority = 100,
@@ -34,17 +31,16 @@ return {
         current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
         sign_priority = 6,
         update_debounce = 100,
-        status_formatter = nil,  -- Use default
-        max_file_length = 40000, -- Disable if file is longer than this (in lines)
+        status_formatter = nil,
+        max_file_length = 40000,
         preview_config = {
-          -- Options passed to nvim_open_win
           border = "single",
           style = "minimal",
           relative = "cursor",
           row = 0,
           col = 1,
         },
-        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hung<CR>", {}),
+        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {}),
         vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {}),
       })
     end,
